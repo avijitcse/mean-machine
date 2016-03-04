218db59eaa253ce3fd8adcfc9014ecedc47dbbc0
Blake Schwartz <blakehschwartz@gmail.com>
2016-03-04 11:26:43 -0800

    Creates CRMApp
    CRMApp is the actual application this tutorial covers. Uses MongoDB,
    Express, Angular and Node.

--- First-App/server.js
+++ First-App/server.js
@@ -1,24 +1,42 @@
 //load the Express package and create app
-var express=require('express');
-var app=express();
-var path=require('path');
+var express = require('express'),
+app = express();
+var path = require('path');
+//adds mongoosejs so we can connect to mongodb
+var mongoose = require('mongoose');
+//connect to mongo database
+mongoose.connect('mongodb://localhost/db_name')
 
 //send index.html to the user for the homepage
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+//login form
+app.route('/login')
+
+    //show the form (GET http://localhost:1337/login)
+    .get(function(req, res){
+        res.send('This is the login form');
+    })
+
+    //process the form (POST http://localhost:1337/login)
+    .post(function(req, res){
+        console.log('processing...');
+        res.send('Processing the login form!');
+    });
+
 //create routes for the admin section:
 
 //get an instance of the router
-var adminRouter=express.Router();
+var adminRouter = express.Router();
 
 //route middleware that will happen on every request
 adminRouter.use(function(req, res, next){
-    
+
     //log each request to the console
     console.log(req.method, req.url);
-    
+
     //continue what we were doing and go to the route
     next();
 });
@@ -37,7 +55,7 @@ adminRouter.get('/users', function(req, res){
 adminRouter.param('name', function(req, res, next, name){
     //name validation here
     console.log('doing name validation on ' + name);
-    
+
     //once the validation is done save the new item in the req
     req.name=name;
     //go to the next item
